f8703100992c87f41a3fc4849f8078817fada714
Wawa <whabimana@gmail.com>
2018-05-31 13:01:15 +0800

    replacing some of pictures

--- project2/canvas-common.js
+++ project2/canvas-common.js
@@ -8,7 +8,7 @@ let dragging = false;
 $('#canvas-draft').mousedown(function(e){
     let mouseX = e.pageX - this.offsetLeft;
     let mouseY = e.pageY - this.offsetTop;
-    currentFunction.onMouseDown([mouseX,mouseY],e);
+    currentFunction.onMouseDown([mouseX,mouseY],e);s
     dragging = true;
 });
 $('#canvas-draft').mousemove(function(e){
@@ -46,3 +46,27 @@ class PaintFunction{
     onMouseLeave(){}
     onMouseEnter(){}
 }    
+var canvas = document.querySelector('canvas');
+/*var ctx = canvas.getContext('2d');
+
+ctx.fillStyle = 'white';
+ctx.font = 'bold 40px Monospace';
+ctx.textBaseline = 'top';
+ctx.fillText('hello', 10, 10);  */
+
+function resize() {
+	// Our canvas must cover full height of screen
+	// regardless of the resolution
+	var height = window.innerHeight;
+	
+	// So we need to calculate the proper scaled width
+	// that should work well with every resolution
+	var ratio = canvas.width/canvas.height;
+	var width = height * ratio;
+	
+	canvas.style.width = width+'px';
+	canvas.style.height = height+'px';
+}
+
+window.addEventListener('load', resize, false);
+window.addEventListener('resize', resize, false);
